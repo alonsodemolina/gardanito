414ada8f395754fc362142bc78a51c6b1d0b9fc2
Nancho Alvarez <vag@uma.es>
2018-03-14 23:53:48 +0100

    Tokenizar

--- src/lily.py
+++ src/lily.py
@@ -32,40 +32,108 @@ def extraer(texto, string):
             llaves=llaves-1
         i=i+1
     resto=texto[:string_start+1] + texto[i:]
-    return (bloque, resto)
+    return bloque, resto
 
 def tokenizar(bloque):
     l=[]
-    i=13
-    hay_caracteres=1
-    while hay_caracteres:
-        i=i+1
+    i=1
+    while True:
         c=bloque[i]
         if c=='}':
-            hay_caracteres=0
-        if c==' ' or c=='\t' or c=='\n':
-            continue
-        if c=='\\':
+            break
+        elif c.isspace():
             i=i+1
-            if bloque[i].isalpha():
-                palabra=""
-                letra=bloque[i]
-                while letra.isalpha():
-                    palabra=palabra + letra
-                    i=i+1
-                    letra=bloque[i]
-                l.append('\\' + palabra)
-                i=i-1
-            else:
-                l.append('\\' + bloque[i])
-            continue 
-        l.append(c)
+            continue
+        elif c.isalpha():
+            token, long = leerpalabra(bloque, i)
+        elif c.isdigit():
+            token, long = leernumero(bloque, i)
+        elif c=='*':
+            token, long = leerproporcion(bloque, i)
+        elif c in {'\'', ',', '.', '-', '_'}:
+            token, long = leersignos(bloque, i, c)
+        elif c=='\\':
+            token, long = leercomando(bloque, i)
+        elif c=='{':
+            token, long = leerbloque(bloque, i)
+        else:
+            token=c
+            long=1
+        l.append(token)
+        i=i+long
     return l
+
+def leerpalabra(string, pos):
+    palabra=""
+    j=pos
+    letra=string[j]
+    while letra.isalpha():
+        palabra=palabra + letra
+        j=j+1
+        letra=string[j]
+    return palabra, j-pos
+
+def leernumero(string, pos):
+    numero=""
+    j=pos
+    digito=string[j]
+    while digito.isdigit():
+        numero=numero + digito
+        j=j+1
+        digito=string[j]
+    return numero, j-pos
+
+def leerproporcion(string, pos):
+    proporcion=""
+    j=pos
+    c=string[j]
+    while c.isdigit() or c.isspace() or c=='/' or c=='*':
+        if not c.isspace():
+            proporcion=proporcion + c
+        j=j+1
+        c=string[j]
+    return proporcion, j-pos
+
+def leersignos(string, pos, signo):
+    signos=""
+    j=pos
+    c=string[j]
+    while c==signo:
+        signos=signos + c
+        j=j+1
+        c=string[j]
+    return signos, j-pos
+
+def leercomando(string, pos):
+    j=pos+1
+    c=string[j]
+    if c.isalpha():
+        comando, long=leerpalabra(string,j)
+    else:
+        comando=c
+        long=1
+    return '\\' + comando, long + 1
+
+def leerbloque(string, pos):
+    bloque="{"
+    j=pos+1
+    llaves=1
+    while llaves>0:
+        c=string[j]
+        bloque=bloque+c
+        if c == '{':
+            llaves=llaves+1
+        if c == '}':
+            llaves=llaves-1
+        j=j+1
+    return bloque, j-pos
 
 
 lilyfile=open("../resources/O_Quam_Gloriosum_Est_Regnum.ly")
 texto=quitar_comments(lilyfile.read())
-(cantus, texto)=extraer(texto, "cantus")
+cantus, texto = extraer(texto, "cantus")
 
-tokens=tokenizar(cantus)
+s=cantus.find('{')
+
+tokens=tokenizar(cantus[s:])
 print tokens
