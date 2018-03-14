166c7d0d1e7ab89ea87cc6c6b5a892fb521df010
Nancho Alvarez <vag@uma.es>
2018-03-14 15:17:46 +0100

    The function extraer now returns two strings

--- src/lily.py
+++ src/lily.py
@@ -11,26 +11,31 @@ def quitar_comments(string):
     return t
 
 def extraer(texto, string):
-    t=""
+    # Busca en texto una definicion de la forma string={....}
+    # Devuelve el bloque {...}, y el texto con la definicion eliminida
+    bloque=""
     # \W matches any non-alphanumeric
     p=re.compile('\W' + string + '\s*=\s*')
     m=p.search(texto)
-    i=m.end()
+    string_start=m.start()
+    string_end=m.end()
+    i=string_end
     llaves=0
-    principio=1
-    while llaves!=0 or principio == 1:
+    initial_status=1
+    while llaves!=0 or initial_status == 1:
         c=texto[i]
-        t=t + c
+        bloque=bloque + c
         if c == '{':
-            principio=0
+            initial_status=0
             llaves=llaves+1
         if c == '}':
             llaves=llaves-1
         i=i+1
-    return t
+    resto=texto[:string_start+1] + texto[i:]
+    return (bloque, resto)
 
 
 lilyfile=open("../resources/O_Quam_Gloriosum_Est_Regnum.ly")
 texto=quitar_comments(lilyfile.read())
-cantus=extraer(texto, "cantus")
+(cantus, texto)=extraer(texto, "cantus")
 print cantus
